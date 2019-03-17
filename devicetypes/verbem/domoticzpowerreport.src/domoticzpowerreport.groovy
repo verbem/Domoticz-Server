d4f1d4f3784989c154374f2bff6198b0e847f0f7
Martin Verbeek <verbeek_martin@hotmail.com>
2019-03-17 10:53:12 +0100

    7.29 power changes

--- devicetypes/verbem/domoticzpowerreport.src/domoticzpowerreport.groovy
+++ devicetypes/verbem/domoticzpowerreport.src/domoticzpowerreport.groovy
@@ -11,12 +11,10 @@
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
+ *	
+ * 7.29 consumptionLow from DZ Server and integers
  *
- */
- 
-preferences {
-    input(name:"prefEnergy", type:"number", title: "Consumed kWh", description:"Energy consumption in kWh")
-}   
+ */  
 metadata {
 	definition (name: "domoticzPowerReport", namespace: "verbem", author: "Martin Verbeek") {
 		capability "Sensor"
@@ -28,12 +26,6 @@ metadata {
         capability "Power Consumption Report"
         capability "Energy Meter"
         capability "Image Capture"
-        
-        attribute "powerTotal", "string"
-        
-        command takeDay
-        command takeMonth
-        command takeYear
         }
         
 	tiles(scale: 2) {       
@@ -50,18 +42,18 @@ metadata {
                 ]
             }
             tileAttribute("device.energyMeter", key: "SECONDARY_CONTROL") {
-                attributeState "energyMeter", label:'${currentValue} kWh', unit: "kWh"
+                attributeState "energyMeter", label:'${currentValue}kWh', unit: "kWh"
             }
         }
         
         valueTile("consumptionHigh", "device.consumptionHigh", width: 2, height: 2) {
-        	state "consumptionHigh", label:'High ${currentValue} W', defaultState: true
+        	state "consumptionHigh", label:'High ${currentValue}W', defaultState: true
     	}
         valueTile("consumptionLow", "device.consumptionLow", width: 2, height: 2) {
-        	state "consumptionLow", label:'Low ${currentValue} W', defaultState: true
+        	state "consumptionLow", label:'Low ${currentValue}W', defaultState: true
     	}
         valueTile("momentaryUsage", "device.momentaryUsage", width: 2, height: 2) {
-        	state "momentaryUsage", label:'Usage ${currentValue} W', defaultState: true
+        	state "momentaryUsage", label:'Usage ${currentValue}W', defaultState: true
     	}
 
 	}
@@ -80,7 +72,6 @@ def installed() {
 
 def updated() {
 	initialize()
-    if (settings.prefEnergy) sendEvent(name: "energy", value: prefEnergy.toDouble())
 }
 
 def initialize() {
@@ -91,121 +82,5 @@ def initialize() {
     else {
     	log.error "You cannot use this DTH without the related SmartAPP Domoticz Server, the device needs to be a child of this App"
         sendEvent(name: "powerTotal", value: "Error", descriptionText: "$device.displayName You cannot use this DTH without the related SmartAPP Domoticz Server", isStateChange: true)
-    }
-}
-
-def takeDay() {
-	def type = device.deviceNetworkId.split(":")[1].split()[0]
-    def copyState 
-	state.chd = "t:"
-    state.chxl = "0:%7C"
-	state.chtt = "24%20hours"
-    state.chco = "0000FF"
-    state.chdl = device.currentState("power").unit
-    
-    if (type == "Power") {
-        if (!parent.state.reportPowerDay || parent.state.reportPowerDay.size() == 0) return
-		copyState = parent.state.reportPowerDay        
-    }
-    else if (type == "Gas") {
-        if (!parent.state.reportGasDay || parent.state.reportGasDay.size() == 0) return
-    	copyState = parent.state.reportGasDay
-    }
-    
-    copyState.sort().each { key, item ->
-        state.chd = state.chd + item + "," 
-        state.chxl = state.chxl + key.split()[0] + "%20" + key.split()[1] + "%7C"
-    }
-    
-    state.chd = state.chd + 0
-    state.chxl = state.chxl + "%7C"
-    take()
-}
-
-def takeMonth() {
-	def type = device.deviceNetworkId.split(":")[1].split()[0]
-    def copyState 
-	state.chd = "t:"
-    state.chxl = "0:%7C"
-	state.chtt = "Month"
-    state.chco = "0000FF"
-    state.chdl = device.currentState("powerTotal").unit
-
-    if (type == "Power") {
-        if (!parent.state.reportPowerMonth || parent.state.reportPowerMonth.size() == 0) return
-		copyState = parent.state.reportPowerMonth        
-    }
-    else if (type == "Gas") {
-        if (!parent.state.reportGasMonth || parent.state.reportGasMonth.size() == 0) return
-    	copyState = parent.state.reportGasMonth
-    }
-    
-    copyState.sort().each { key, item ->
-        state.chd = state.chd + item + "," 
-        state.chxl = state.chxl + key + "%7C"
-    }
-    
-    state.chd = state.chd + 0
-    state.chxl = state.chxl + "%7C"
-    take()
-}
-
-def takeYear() {
-	def type = device.deviceNetworkId.split(":")[1].split()[0]
-    def copyState 
-	state.chd = "t:"
-    state.chxl = "0:%7C"
-	state.chtt = "Year"
-    state.chco = "0000FF"
-    state.chdl = device.currentState("powerTotal").unit
-    
-    if (type == "Power") {
-        if (!parent.state.reportPowerYear || parent.state.reportPowerYear.size() == 0) return
-		copyState = parent.state.reportPowerYear        
-    }
-    else if (type == "Gas") {
-        if (!parent.state.reportGasYear || parent.state.reportGasYear.size() == 0) return
-    	copyState = parent.state.reportGasYear
-    }
-    
-    copyState.sort().each { key, item ->
-        state.chd = state.chd + item + "," 
-        state.chxl = state.chxl + key + "%7C"
-    }
-    
-    state.chd = state.chd + 0
-    state.chxl = state.chxl + "%7C"
-    take()
-}
-
-def take() {
-	return
-    
-	def imageCharts = "https://image-charts.com/chart?"
-	def params = [uri: "${imageCharts}chs=720x480&chd=${state.chd}&cht=bvg&chds=a&chxt=x,y&chxl=${state.chxl}&chts=0000FF,20&chco=${state.chco}&chtt=${state.chtt}&chdl=${state.chdl}"]
-    
-    if (state.imgCount == null) state.imgCount = 0
- 
-    try {
-        httpGet(params) { response ->
-        	
-            if (response.status == 200 && response.headers.'Content-Type'.contains("image/png")) {
-                def imageBytes = response.data
-                if (imageBytes) {
-                    state.imgCount = state.imgCount + 1
-                    def name = "PowerUsage$state.imgCount"
-
-                    // the response data is already a ByteArrayInputStream, no need to convert
-                    try {
-                        storeImage(name, imageBytes, "image/png")
-                    } catch (e) {
-                        log.error "error storing image: $e"
-                    }
-                }
-            }
-        else log.error "wrong format of content"
-        }
-    } catch (err) {
-        log.error ("Error making request: $err")
     }
 }
